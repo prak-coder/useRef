59f45398838c5b5891c818c054de68be8df9f06d
Prakash K <prakash.change@gmail.com>
2026-04-15 12:25:27 +0530

    catfriends scroll img to view useRef

--- vite-useRef/src/App.jsx
+++ vite-useRef/src/App.jsx
@@ -1,10 +1,14 @@
 import { useRef, useState } from "react";
+import cat1 from "./assets/cat1.jfif";
+import cat2 from "./assets/cat2.jfif";
+import cat3 from "./assets/cat3.jfif";
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col gap-1">
       <StopWatch />
       <Form />
+      <CatFriends />
     </div>
   );
 }
@@ -38,12 +42,28 @@ function StopWatch() {
     setNow(Date.now());
   }
   return (
-    <div>
+    <div className="flex flex-row gap-2">
       <p>time passed: {time.toFixed(3)}</p>
-      <button onClick={isStart ? handleStart : handleStop}>
+      <button
+        className="px-4 py-2 rounded-lg font-medium bg-blue-500 text-white 
+hover:bg-blue-600 active:scale-95 
+focus:outline-none focus:ring-2 focus:ring-blue-400 
+disabled:opacity-50 disabled:cursor-not-allowed 
+transition duration-200"
+        onClick={isStart ? handleStart : handleStop}
+      >
         {isStart ? "Start" : "Stop"}
       </button>
-      <button onClick={handleClearTime}>clear</button>
+      <button
+        className="px-4 py-2 rounded-lg font-medium bg-blue-500 text-white 
+hover:bg-blue-600 active:scale-95 
+focus:outline-none focus:ring-2 focus:ring-blue-400 
+disabled:opacity-50 disabled:cursor-not-allowed 
+transition duration-200"
+        onClick={handleClearTime}
+      >
+        clear
+      </button>
       {/* <button onClick={handleStop}>Stop</button> */}
     </div>
   );
@@ -55,8 +75,56 @@ function Form() {
   }
   return (
     <>
-      <input type="text" ref={inputRef} />
-      <button onClick={handleFocus}>Focus the input</button>
+      <p className="bg-green-300">Form</p>
+      <input className="h-10 w-150" type="text" ref={inputRef} />
+      <button
+        className="px-4 py-2 rounded-lg font-medium bg-blue-500 text-white 
+hover:bg-blue-600 active:scale-95 
+focus:outline-none focus:ring-2 focus:ring-blue-400 
+disabled:opacity-50 disabled:cursor-not-allowed 
+transition duration-200 h-10 w-150 "
+        onClick={handleFocus}
+      >
+        Focus the input
+      </button>
+    </>
+  );
+}
+function CatFriends() {
+  return (
+    <>
+      <p className="text-center">CatFriends</p>
+      <nav>
+        <button
+          className="px-4 py-2 rounded-lg font-medium bg-blue-500 text-white 
+hover:bg-blue-600 active:scale-95"
+        >
+          cat1
+        </button>
+        <button
+          className="px-4 py-2 m-2 rounded-lg font-medium bg-blue-500 text-white 
+hover:bg-blue-600 active:scale-95"
+        >
+          cat2
+        </button>
+        <button
+          className="px-4 py-2 rounded-lg font-medium bg-blue-500 text-white 
+hover:bg-blue-600 active:scale-95"
+        >
+          cat3
+        </button>
+      </nav>
+      <ul className="flex gap-10">
+        <li>
+          <img src={cat1} alt="cat1" />
+        </li>
+        <li>
+          <img src={cat2} alt="cat2" />
+        </li>
+        <li>
+          <img src={cat3} alt="cat3" />
+        </li>
+      </ul>
     </>
   );
 }
